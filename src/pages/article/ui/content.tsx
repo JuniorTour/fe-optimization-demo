da12175db566edede76e8c93128e5f641e3c7ac3
juniortour <juniortour@qq.com>
2023-10-07 20:58:30 +0800

    feat: lazyLoad NPM package

--- src/pages/article/ui/content.tsx
+++ src/pages/article/ui/content.tsx
@@ -1,9 +1,33 @@
-import Markdown from 'markdown-to-jsx';
+// import Markdown from 'markdown-to-jsx';
 import { Row } from '@/shared/ui';
 import { selectors } from '../model';
 import { Tags } from './tags';
 
+let Markdown = null;
+let startLoad = false;
+
+function lazyLoadMarkdownToJSX() {
+  if (startLoad) {
+    return;
+  }
+  startLoad = true;
+  import(
+    /* webpackChunkName: "markdown-to-jsx" */
+    'markdown-to-jsx'
+  )
+    .then((module) => {
+      Markdown = module.default;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
 export const Content = () => {
+  lazyLoadMarkdownToJSX();
+  if (!Markdown) {
+    return null;
+  }
   const { body } = selectors.useArticle();
 
   return (
