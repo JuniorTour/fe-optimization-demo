d7775c2512e2f286622f4d87731b452512276188
juniortour <juniortour@qq.com>
2023-10-15 23:17:06 +0800

    feat: granular code split

--- webpack/production.config.js
+++ webpack/production.config.js
@@ -5,7 +5,25 @@ const TerserPlugin = require('terser-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const crypto = require('crypto');
+const path = require('path');
 const { DIST, SRC } = require('./constants');
+
+const MAX_REQUEST_NUM = 20;
+// 指定一个 module 可以被拆分为独立 区块（chunk） 的最小源码体积（单位：byte）
+const MIN_LIB_CHUNK_SIZE = 10 * 1000;
+
+const isModuleCSS = (module) => {
+  return (
+    // mini-css-extract-plugin
+    module.type === `css/mini-extract` ||
+    // extract-css-chunks-webpack-plugin (old)
+    module.type === `css/extract-chunks` ||
+    // extract-css-chunks-webpack-plugin (new)
+    module.type === `css/extract-css-chunks`
+  );
+};
 
 module.exports = {
   mode: 'production',
@@ -15,17 +33,62 @@ module.exports = {
     filename: '[name].[contenthash].js',
   },
   optimization: {
-    moduleIds: 'deterministic',
+    // moduleIds: 'deterministic',
     // https://webpack.js.org/configuration/optimization/#optimizationruntimechunk
     // 指定是否将Webpack的运行时（每个文件中重复的、用于加载的函数）拆分为独立文件，能减少重复代码。
     runtimeChunk: 'single',
     splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: MAX_REQUEST_NUM,
+      maxAsyncRequests: MAX_REQUEST_NUM,
       cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          // chunks: 'all',    // 指定vendors区块包含同步、异步加载的2类模块。
-          chunks: 'initial', // 指定vendors区块只包含同步加载的模块。
+        defaultVendors: false,
+        default: false,
+        lib: {
+          chunks: 'all',
+          test(module) {
+            return (
+              module.size() > MIN_LIB_CHUNK_SIZE &&
+              /node_modules[/\\]/.test(module.identifier())
+            );
+          },
+          name(module) {
+            const hash = crypto.createHash('sha1');
+            if (isModuleCSS(module)) {
+              module.updateHash(hash);
+            } else {
+              if (!module.libIdent) {
+                throw new Error(
+                  `Encountered unknown module type: ${module.type}. Please check webpack/prod.client.config.js.`,
+                );
+              }
+              hash.update(
+                module.libIdent({ context: path.join(__dirname, '../') }),
+              );
+            }
+
+            return `lib_${hash.digest('hex').substring(0, 8)}`;
+          },
+          priority: 3,
+          minChunks: 1,
+          reuseExistingChunk: true,
+        },
+        shared: {
+          chunks: 'all',
+          minSize: MIN_LIB_CHUNK_SIZE,
+          name(module, chunks) {
+            return `shared_${crypto
+              .createHash('sha1')
+              .update(
+                chunks.reduce((acc, chunk) => {
+                  return acc + chunk.name;
+                }, ''),
+              )
+              .digest('hex')}${isModuleCSS(module) ? '_CSS' : ''}`;
+          },
+          priority: 1,
+          minChunks: 2,
+          reuseExistingChunk: true,
         },
       },
     },
